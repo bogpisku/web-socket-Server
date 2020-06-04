5707ac51df5e85a5eb6dfc9310cad30232dcb93d
bogpisku <bogpisku@gmail.com>
2020-06-03 23:41:14 -0500

    first commit working

---
+++ server/main.js
@@ -0,0 +1,20 @@
+var WebSocketServer = require('ws').Server
+, wss = new WebSocketServer({ port: 8080 }); // If you want to add a path as well, use path: "PathName"
+
+const fs = require('fs');
+var streamGravData = fs.createWriteStream("grav.csv", {flags:'a'});
+var streamGyroData = fs.createWriteStream("gyro.csv", {flags:'a'});
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    var jsonObj=JSON.parse(message);
+    if(jsonObj.type==='grav'){
+        console.log('grav received')
+        streamGravData.write(jsonObj.data.x+","+jsonObj.data.y+","+jsonObj.data.z+"\n")
+    }
+    if(jsonObj.type==='gyro'){
+      console.log('gyro received');
+      streamGyroData.write(jsonObj.data.x+","+jsonObj.data.y+","+jsonObj.data.z+"\n")
+    }
+  });
+});
+
