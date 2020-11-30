6f5a10c34a58f37e4bb944f1490c19b0f07f78f6
bogpisku <bogpisku@gmail.com>
2020-11-29 20:01:51 -0600

    add data Sets

--- server/main.js
+++ server/main.js
@@ -1,28 +1,28 @@
 var WebSocketServer = require('ws').Server
 , wss = new WebSocketServer({ port: 8080 }); // If you want to add a path as well, use path: "PathName"
-
+const kick="steap";
 const fs = require('fs');
-var streamGravData = fs.createWriteStream("grav.csv", {flags:'a'});
-var streamGyroData = fs.createWriteStream("gyro.csv", {flags:'a'});
-var streamAccData = fs.createWriteStream("acc.csv", {flags:'a'});
-var streamLAccData = fs.createWriteStream("lacc.csv", {flags:'a'});
+var streamGravData = fs.createWriteStream(kick+"grav.csv", {flags:'a'});
+var streamGyroData = fs.createWriteStream(kick+"gyro.csv", {flags:'a'});
+var streamAccData = fs.createWriteStream(kick+"acc.csv", {flags:'a'});
+var streamLAccData = fs.createWriteStream(kick+"lacc.csv", {flags:'a'});
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     var jsonObj=JSON.parse(message);
     if(jsonObj.type==='grav'){
-        console.log('grav received')
+       // console.log('grav received')
         streamGravData.write(jsonObj.data.x+","+jsonObj.data.y+","+jsonObj.data.z+"\n")
     }
     if(jsonObj.type==='gyro'){
-      console.log('gyro received');
+      //console.log('gyro received');
       streamGyroData.write(jsonObj.data.x+","+jsonObj.data.y+","+jsonObj.data.z+"\n")
     }
     if(jsonObj.type==='acc'){
-      console.log('acc received');
+      //console.log('acc received');
       streamAccData.write(jsonObj.data.x+","+jsonObj.data.y+","+jsonObj.data.z+"\n")
     }
     if(jsonObj.type==='lacc'){
-      console.log('acc received');
+      console.log('lacc received');
       streamLAccData.write(jsonObj.data.x+","+jsonObj.data.y+","+jsonObj.data.z+"\n")
     }
   });
